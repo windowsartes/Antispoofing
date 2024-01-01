cc3c587e5a41a494e7997303f4a9efba8a501c22
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-01-01 19:39:10 +0300

    code refactoring

--- trainYOLO.py
+++ trainYOLO.py
@@ -4,17 +4,23 @@ from pathlib import Path
 from ultralytics import YOLO
 
 
-file_path: os.PathLike = Path(os.path.basename(__file__))
 dir_path: os.PathLike = Path(os.path.dirname(os.path.realpath(__file__)))
-file_path = Path.joinpath(dir_path, file_path)
 
-dataYamlPath: os.PathLike = Path.joinpath(file_path.parents[0], "Datasets/SplitData/data.yaml")
+data_yaml_path: os.PathLike = Path.joinpath(Path.joinpath(Path.joinpath(
+    Path.joinpath(dir_path, "Datasets"), "SplitData"), "data.yaml"))
 
 def train(model: YOLO, num_epochs: int = 10):
-    model.train(data=dataYamlPath, epochs=num_epochs)
+    """Trains given model on given number of epochs
+
+    :param model: model you want to train
+    :type model: YOLO
+    :param num_epochs: number of epochs during which you want to train your model, defaults to 10
+    :type num_epochs: int, optional
+    """
+    model.train(data=data_yaml_path, epochs=num_epochs)
 
 
 if __name__ == "__main__":
     model: YOLO = YOLO("yolov8n.pt")
 
-    train(model, 10)
+    train(model, 20)
